bc8556d5af89451d8ae57143711e0369f5ed4b34
Abigail Adegbiji <115902132+aabiji@users.noreply.github.com>
2024-03-13 21:10:17 +0000

    solved second extra step

---
+++ reeborg/extra-step-2.py
@@ -0,0 +1,34 @@
+"""
+Solution for "Extra - Find the Centre 2"
+Find the geometric center of the grid.
+
+Author: Abigail Adegbiji
+Date: March 13
+"""
+
+import library as lib
+
+# Find the width
+width = 0
+while not wall_in_front():
+    width += 1
+    move()
+
+turn_left()
+
+# Find the height
+height = 0
+while not wall_in_front():
+    height += 1
+    move()
+
+lib.turn(2)
+
+# Go to center
+for y in range(height // 2):
+    move()
+lib.turn_right()
+for x in range(width // 2):
+    move()
+
+put()
