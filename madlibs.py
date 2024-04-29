6b8d2aba26df621aa8fd775cc246acc6bae93884
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-04-28 22:00:08 -0600

    understand gui

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,6 @@
 # Madlibs
 # By Abigail Adegbiji, April 26, 2024
-
-import tkinter as tk
+import PySimpleGUI as gui
 
 # Return a 2d array containing each word in each sentence
 def split_story(story_str):
@@ -15,6 +14,21 @@ def split_story(story_str):
         story.append(words)
     return story
 
+# Build a the story ui. The inputs go on the left
+# and the text itself goes on the right
+def build_story_ui(prompts, story):
+    inputs = []
+    for p in prompts:
+        layout = [gui.Text(p), gui.InputText(size=25, key=p)]
+        inputs.append(layout)
+
+    buttons = [gui.Button("Generate")]
+    inputs.append(buttons)
+
+    text = [[gui.Text(" ".join(line))] for line in story]
+
+    return [[gui.Column(inputs), gui.Column(text, id="Text")]]
+
 humpty_dumpty = """
 Humpty Dumpty sat on a wall,
 Humpty Dumpty had a great fall.
@@ -23,12 +37,37 @@ All the king's horses and all the king's men
 Couldn't put Humpty together again. 
 """
 story = split_story(humpty_dumpty)
+prompts = ["First name", "Last name", "Verb" + " " * 8]
+story1 = build_story_ui(prompts, story)
 
-window = tk.Tk()
+# GUI layout for the main menu
+menu_layout = [
+    [gui.Text("Which story do you want?")],
+    [gui.Button("Humpty Dumpty"), gui.Button("Something else")]
+]
 
-for line in story:
-    line_str = " ".join(line)
-    label = tk.Label(text=line_str)
-    label.pack()
+# All the different column layouts in the gui
+# For context, PySimpleGUI stores layouts as nested lists:
+# https://docs.pysimplegui.com/en/latest/documentation/module/layouts/
+# Start with only having the main menu visible
+layouts = [[
+    gui.Column(menu_layout, key="Main-Menu"),
+    gui.Column(story1, key="Layout-1", visible=False),
+]]
+window = gui.Window("Madlibs", layouts)
 
-window.mainloop()
+while True:
+    event, values = window.read()
+    if event == gui.WINDOW_CLOSED:
+        break
+
+    # Main menu button click
+    if event == "Humpty Dumpty":
+        window["Main-Menu"].update(visible=False)
+        window["Layout-1"].update(visible=True)
+
+    # Generate mad libs button click
+    if event == "Generate":
+        print("hello!")
+
+window.close()
