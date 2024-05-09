a186e716beeafe97ec4b2b8d068cacc08a37207d
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-05-09 12:29:20 -0600

    project proposal

---
+++ microbitio/project/Abigail-Adegbiji-Microbit-Proposal.py
@@ -0,0 +1,16 @@
+"""
+Description
+
+Ideas:
+A recreation of Simon Says. There will be 2 different patterns that are used.
+A sequence of patterns will be shown to the player. The player needs to play
+back those patterns in the same order they appeared. The player will choose
+the first pattern with the A button and the second pattern with the B button.
+When the player successfully plays back the pattern, the game progresses to the
+next level and a sound effect is played. The game gets harder with each level
+with longer sequences of patterns and faster playback times. When the user fails,
+the game displays which level the user made it to and the player's highest level.
+When the user fails, a sound effect is played and the game is restarted from the beginning.
+
+Pseudocode
+"""
